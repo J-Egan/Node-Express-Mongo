76f4f4a5c4486a83387f327dad9a9dd942e82e28
J-Egan <jackegan248@gmail.com>
2021-09-29 23:10:22 +0100

    2 files modified, 2 added, 1 deleted

---
+++ server.js
@@ -0,0 +1,21 @@
+const bodyParser= require('body-parser')
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const app = express();
+
+MongoClient.connect('mongodb-connection-string', (err, client) => {
+  // ... do something here
+})
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.listen(3000, function() {
+  console.log('listening on 3000');
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/quotes', (req, res) => {
+  console.log(req.body);
+})
